fc998bdb1c5b1cb9790fe60706cf4d3fc9a15f50
JulianaForbici <julianaforbicii@gmail.com>
2025-12-03 22:44:02 -0300

    feat: ajustado o healthcontroller para a api constar como 'API conectada com sucesso' no frontend

--- backend/src/main/java/br/com/blumenau/backend/HealthController.java
+++ backend/src/main/java/br/com/blumenau/backend/HealthController.java
@@ -1,9 +1,11 @@
 package br.com.blumenau.backend;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000") // libera o front em dev
 public class HealthController {
 
     @GetMapping("/health")
