689bae40c75842b756702189ea26d038dc76421c
JulianaForbici <julianaforbicii@gmail.com>
2025-12-03 22:28:14 -0300

    fix: ajustado o controller e o backend com os pacotes corretos para o backend rodar em docker

--- backend/src/main/java/br/com/blumenau/backend/BackendApplication.java
+++ backend/src/main/java/br/com/blumenau/backend/BackendApplication.java
@@ -3,11 +3,10 @@ package br.com.blumenau.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.blumenau.backend")
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
